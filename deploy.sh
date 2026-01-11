f752ca6640ddb01c6c9306bcc842d9b1cc962ac4
AdithyaLaakso <adithya.laakso@gmail.com>
2026-01-10 21:00:38 -0500

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 cd "$(dirname "$0")"
-docker build --push -t thavlik/psyslop-guide:latest .
+docker build -t adithyalaakso/psyslop-client:latest -f ./Dockerfile .
+docker push adithyalaakso/psyslop-client
 kubectl rollout restart deployment apps-guide -n apps
 k9s -n guide -c pods --headless --splashless --context do-nyc1-beebs
